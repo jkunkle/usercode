e0c178652ea0489ddc1974eb7d5ef3771e221f86
Josh Kunkle <jkunkle@cern.ch>
2014-10-23 18:19:37 +0200

    update scheduler

--- Analysis/TreeFilter/FilterWgamgamFinal/scripts/scheduler.py
+++ Analysis/TreeFilter/FilterWgamgamFinal/scripts/scheduler.py
@@ -11,13 +11,13 @@ options = p.parse_args()
 if not options.run and not options.check :
     options.run = True
 
-#base = r'/eos/cms/store/user/jkunkle/Wgamgam/RecoOutputNoEleVetoSCRVars_2014_04_30'
-base = r'/eos/cms/store/user/jkunkle/Wgamgam/RecoOutputNoLepIso_2014_10_15'
+base = r'/eos/cms/store/user/jkunkle/Wgamgam/RecoOutputNoEleVetoSCRVars_2014_04_30'
+#base = r'/eos/cms/store/user/jkunkle/Wgamgam/RecoOutputNoLepIso_2014_10_15'
 jobs_data = [
-        #(base, 'job_electron_2012a_Jan22rereco'),
-        #(base, 'job_electron_2012b_Jan22rereco'),
-        #(base, 'job_electron_2012c_Jan2012rereco'),
-        #(base, 'job_electron_2012d_Jan22rereco'),
+        (base, 'job_electron_2012a_Jan22rereco'),
+        (base, 'job_electron_2012b_Jan22rereco'),
+        (base, 'job_electron_2012c_Jan2012rereco'),
+        (base, 'job_electron_2012d_Jan22rereco'),
         (base, 'job_muon_2012a_Jan22rereco'),
         (base, 'job_muon_2012b_Jan22rereco'),
         (base, 'job_muon_2012c_Jan22rereco'),
@@ -56,53 +56,54 @@ jobs_data = [
 ]
 
 jobs_mc = [
-        #(base, 'job_summer12_DYJetsToLL'),
-        #(base, 'job_summer12_Zg'),
-        #(base, 'job_summer12_WAA_ISR'),
-        #(base, 'job_summer12_Wgg_FSR'),
-        #(base, 'job_summer12_Wg'),
-        #(base, 'job_summer12_Wjets'),
-        #(base, 'job_summer12_ttjets_1l'),
-        #(base, 'job_summer12_ttjets_2l'),
-        #(base, 'job_summer12_ttg'),
-        #(base, 'job_summer12_WgPt130'),
-        #(base, 'job_summer12_WgPt50-130'),
-        #(base, 'job_summer12_WgPt30-50'),
-        #(base, 'job_summer12_WgPt20-30'),
-        #(base, 'job_summer12_DiPhotonBorn_Pt-10To25'),
-        #(base, 'job_summer12_WH_ZH_125'),
-        #(base, 'job_summer12_WWW'),
-        #(base, 'job_summer12_WWZ'),
-        #(base, 'job_summer12_WW_2l2nu'),
-        #(base, 'job_summer12_WWg'),
-        #(base, 'job_summer12_WZZ'),
-        #(base, 'job_summer12_WZ_3lnu'),
-        #(base, 'job_summer12_ZZZ'),
-        #(base, 'job_summer12_ZZ_2e2mu'),
-        #(base, 'job_summer12_ZZ_2e2tau'),
-        #(base, 'job_summer12_ZZ_2l2nu'),
-        #(base, 'job_summer12_ZZ_2l2q'),
-        #(base, 'job_summer12_ZZ_2mu2tau'),
-        #(base, 'job_summer12_ZZ_2q2nu'),
-        #(base, 'job_summer12_ZZ_4e'),
-        #(base, 'job_summer12_ZZ_4mu'),
-        #(base, 'job_summer12_ZZ_4tau'),
-        #(base, 'job_summer12_diphoton_box_10to25'),
-        #(base, 'job_summer12_diphoton_box_250toInf'),
-        #(base, 'job_summer12_diphoton_box_25to250'),
-        #(base, 'job_summer12_ggZZ_2l2l'),
-        #(base, 'job_summer12_ggZZ_4l'),
-        #(base, 'job_summer12_t_s'),
-        #(base, 'job_summer12_t_t'),
-        #(base, 'job_summer12_t_tW'),
-        #(base, 'job_summer12_tbar_s'),
-        #(base, 'job_summer12_tbar_t'),
-        #(base, 'job_summer12_tbar_tW'),
-        #(base, 'job_summer12_ttinclusive'),
-        #(base, 'QCD_Pt-40_doubleEMEnriched')
-        ##(base_tmp, 'DYJetsToLLPhOlap' )
-        ##(base_tmp, 'WjetsPhOlap' )
-        ##(base_tmp, 'WgPhOlap' )
+        (base, 'job_summer12_DYJetsToLL'),
+        (base, 'job_summer12_Zg'),
+        (base, 'job_summer12_Zgg'),
+        (base, 'job_summer12_WAA_ISR'),
+        (base, 'job_summer12_Wgg_FSR'),
+        (base, 'job_summer12_Wg'),
+        (base, 'job_summer12_Wjets'),
+        (base, 'job_summer12_ttjets_1l'),
+        (base, 'job_summer12_ttjets_2l'),
+        (base, 'job_summer12_ttg'),
+        (base, 'job_summer12_WgPt130'),
+        (base, 'job_summer12_WgPt50-130'),
+        (base, 'job_summer12_WgPt30-50'),
+        (base, 'job_summer12_WgPt20-30'),
+        (base, 'job_summer12_DiPhotonBorn_Pt-10To25'),
+        (base, 'job_summer12_WH_ZH_125'),
+        (base, 'job_summer12_WWW'),
+        (base, 'job_summer12_WWZ'),
+        (base, 'job_summer12_WW_2l2nu'),
+        (base, 'job_summer12_WWg'),
+        (base, 'job_summer12_WZZ'),
+        (base, 'job_summer12_WZ_3lnu'),
+        (base, 'job_summer12_ZZZ'),
+        (base, 'job_summer12_ZZ_2e2mu'),
+        (base, 'job_summer12_ZZ_2e2tau'),
+        (base, 'job_summer12_ZZ_2l2nu'),
+        (base, 'job_summer12_ZZ_2l2q'),
+        (base, 'job_summer12_ZZ_2mu2tau'),
+        (base, 'job_summer12_ZZ_2q2nu'),
+        (base, 'job_summer12_ZZ_4e'),
+        (base, 'job_summer12_ZZ_4mu'),
+        (base, 'job_summer12_ZZ_4tau'),
+        (base, 'job_summer12_diphoton_box_10to25'),
+        (base, 'job_summer12_diphoton_box_250toInf'),
+        (base, 'job_summer12_diphoton_box_25to250'),
+        (base, 'job_summer12_ggZZ_2l2l'),
+        (base, 'job_summer12_ggZZ_4l'),
+        (base, 'job_summer12_t_s'),
+        (base, 'job_summer12_t_t'),
+        (base, 'job_summer12_t_tW'),
+        (base, 'job_summer12_tbar_s'),
+        (base, 'job_summer12_tbar_t'),
+        (base, 'job_summer12_tbar_tW'),
+        (base, 'job_summer12_ttinclusive'),
+        (base, 'QCD_Pt-40_doubleEMEnriched')
+        #(base_tmp, 'DYJetsToLLPhOlap' )
+        #(base_tmp, 'WjetsPhOlap' )
+        #(base_tmp, 'WgPhOlap' )
 ]
 
 #module_mc   = 'ConfLepGammaFilter.py'
@@ -126,11 +127,18 @@ top_configs = [
                 #},
                 { 
                   'module'      : 'ConfObjFilter.py',
-                    'args_mc'     : '{ \'cut_nLepTrigMatch\' : \' > 0 \', \'cut_nLep\' : \'> 0\', \'cut_nPh\' : \'> 1\', \'cut_hasPixSeed_leadph12\' : \'False\', \'cut_hasPixSeed_sublph12\' : \'False\'  }',
-                  'args_data'   : '{ \'cut_nLepTrigMatch\' : \' > 0 \', \'cut_nLep\' : \'> 0\', \'cut_nPh\' : \'> 1\', \'isData\' : \'True\', \'cut_hasPixSeed_leadph12\' : \'False\', \'cut_hasPixSeed_sublph12\' : \'False\' }',
-                  'output_name' : 'LepGammaGammaNoPhIDVetoPixSeedBothNoLepIso_2014_10_16',
+                    'args_mc'     : '{ \'cut_nLepTrigMatch\' : \' > 0 \', \'cut_nLep\' : \'> 0\', \'cut_nPh\' : \'> 1\' }',
+                  'args_data'   : '{ \'cut_nLepTrigMatch\' : \' > 0 \', \'cut_nLep\' : \'> 0\', \'cut_nPh\' : \'> 1\', \'isData\' : \'True\' }',
+                  'output_name' : 'LepGammaGammaNoPhID_2014_10_23',
                  'tag'         : 'lgg',
                 },
+                #{ 
+                #  'module'      : 'ConfObjFilter.py',
+                #    'args_mc'     : '{ \'cut_nLepTrigMatch\' : \' > 0 \', \'cut_nLep\' : \'> 0\', \'cut_nPh\' : \'> 1\', \'cut_hasPixSeed_leadph12\' : \'False\', \'cut_hasPixSeed_sublph12\' : \'False\'  }',
+                #  'args_data'   : '{ \'cut_nLepTrigMatch\' : \' > 0 \', \'cut_nLep\' : \'> 0\', \'cut_nPh\' : \'> 1\', \'isData\' : \'True\', \'cut_hasPixSeed_leadph12\' : \'False\', \'cut_hasPixSeed_sublph12\' : \'False\' }',
+                #  'output_name' : 'LepGammaGammaNoPhIDVetoPixSeedBoth_2014_10_22_v2',
+                # 'tag'         : 'lgg',
+                #},
                 #{ 
                 #  'module'      : 'ConfObjFilter.py',
                 #    'args_mc'     : '{ \'cut_nLepTrigMatch\' : \' > 0 \', \'cut_nLep\' : \'> 0\', \'cut_nPh\' : \'> 1\', \'cut_hasPixSeed_leadph12\' : \'True\', \'cut_hasPixSeed_sublph12\' : \'False\'  }',
@@ -231,7 +239,7 @@ check_commands_base = 'python ../../Util/scripts/check_dataset_completion.py --o
 #command_base = 'python scripts/filter.py  --filesDir %(base)s/%(job)s --fileKey tree.root --outputDir /afs/cern.ch/work/j/jkunkle/private/CMS/Wgamgam/Output/%(output)s/%(job)s --outputFile tree.root --treeName ggNtuplizer/EventTree --module scripts/%(module)s --moduleArgs "%(moduleArgs)s" --nFilesPerJob 5 --nproc %(nproc)d --confFileName %(tag)s_%(job)s.txt --exeName %(exe)s'
 
 nFilesPerJob = 2
-nproc=8
+nproc=5
 treename='ggNtuplizer/EventTree'
 
 if options.run :

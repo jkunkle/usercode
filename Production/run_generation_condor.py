18c021987ef22bd5b568a867e69e806852914a4c
Josh Kunkle <jkunkle@cern.ch>
2016-10-07 13:31:07 +0200

    update submission code

--- Production/run_generation_condor.py
+++ Production/run_generation_condor.py
@@ -2,15 +2,19 @@ from argparse import ArgumentParser
 import os
 from uuid import uuid4
 import subprocess
-
+import getpass
 parser = ArgumentParser( )
 
 parser.add_argument('--prod_dir', dest='prod_dir', required=True, help='path to processing directory' )
 parser.add_argument('--gridpack', dest='gridpack', required=True, help='path to gridpack' )
 parser.add_argument('--nevt', dest='nevt', type=int, default=20000, help='number of events to run.  Each job will contain 1000 events' )
 parser.add_argument('--nevtPerJob', dest='nevtPerJob', type=int, default=1000, help='number of events to run per job default = 1000' )
+parser.add_argument('--lxbatch', dest='lxbatch', default=False, action='store_true', help='submit to lxbatch' )
+parser.add_argument('--lxqueue', dest='lxqueue', default='1nd',  help='lxbatch queue' )
 
 options = parser.parse_args()
+
+PWD = os.path.dirname(os.path.realpath(__file__))
 
 
 def main() :
@@ -21,9 +25,9 @@ def main() :
 
 
     # no longer need certificate
-    #print 'FIRST MAKE A GRID CERTIFICATE'
-    #os.system( 'voms-proxy-init -voms cms -rfc ' )
-
+    if options.lxbatch :
+        print 'FIRST MAKE A GRID CERTIFICATE'
+        os.system( 'voms-proxy-init -voms cms -rfc ' )
 
     if not os.path.isdir( options.prod_dir ) :
         os.makedirs ( options.prod_dir )
@@ -44,6 +48,15 @@ def main() :
         if splitline[0].count('path') :
             proxy_path = splitline[1]
             break
+
+    # if running on lxbatch copy the
+    # proxy to afs
+    if options.lxbatch :
+        username = getpass.getuser()
+        new_proxy_path = '/afs/cern.ch/user/%s/%s' %( username[0], username )
+
+        os.system( 'cp %s %s ' %( proxy_path, new_proxy_path ) )
+
             
     job_desc_text.append( '#Use only the vanilla universe' )
     job_desc_text.append( 'universe = vanilla' )
@@ -69,7 +82,15 @@ def main() :
         if not os.path.isdir( '%s/%s' %( options.prod_dir, job_dir ) ) :
             os.makedirs( '%s/%s' %( options.prod_dir, job_dir ) ) 
 
-        os.system( 'python /home/jkunkle/Programs/run_all_generation_steps.py  --prod_dir %s/%s --name %s --gridpack %s  --nevt %d --writeShellScript --scriptName %s ' %( options.prod_dir, job_dir, output_name, options.gridpack, options.nevtPerJob, scriptName) )
+        generation_text = 'python %s/run_all_generation_steps.py  --prod_dir %s/%s --name %s --gridpack %s  --nevt %d --writeShellScript --scriptName %s ' %( PWD, options.prod_dir, job_dir, output_name, options.gridpack, options.nevtPerJob, scriptName)
+        if options.lxbatch :
+            proxy_name = os.path.basename( proxy_path )
+            username = getpass.getuser()
+            new_proxy_path = '/afs/cern.ch/user/%s/%s/%s' %( username[0], username, proxy_name )
+            generation_text += ' --proxyPath %s ' %( new_proxy_path )  
+
+
+        os.system( generation_text )
 
         #job_desc_text.append( 'Executable = /home/jkunkle/Programs/run_all_generation_steps.py' )
         job_desc_text.append( 'Executable = %s/%s/%s'%( options.prod_dir, job_dir, scriptName ) )
@@ -90,7 +111,10 @@ def main() :
 
     job_desc_file.close()
 
-    os.system( 'condor_submit %s ' %( job_desc_file_name ) )
+    if options.lxbatch :
+        os.system( 'bsub -q %s %s/%s/%s -o %s/%s/stdout.txt -e %s/%s/stderr.txt ' %( options.lxqueue, options.prod_dir, job_dir, scriptName, options.prod_dir, job_dir, options.prod_dir, job_dir ) )
+    else :
+        os.system( 'condor_submit %s ' %( job_desc_file_name ) )
 
 
 
